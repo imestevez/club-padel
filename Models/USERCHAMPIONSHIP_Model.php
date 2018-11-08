2cc4ee2fb1b44c26abb6480954f019208dad5888
cristinameno <menocristina@gmail.com>
2018-11-08 16:34:46 +0100

    Diego continua

--- Models/USERCHAMPIONSHIP_Model.php
+++ Models/USERCHAMPIONSHIP_Model.php
@@ -8,8 +8,12 @@ class USERCHAMPONSHIP_Model{
 	var $pareja_id;
 	var $campeonato_id;
 
+	var $mysqli;
+
 	function __construct($login){
 		
+		include_once '../Functions/Access_DB.php';
+        $this->mysqli = ConnectDB();
 	}
 
 
@@ -25,8 +29,22 @@ class USERCHAMPONSHIP_Model{
 
 	//Función para obtener los campeonatos en los que participa un usuario
 	function SHOWALL(){
-
 		//Buscamos parejas registradas del usuario
+		$sql_par = "SELECT * FROM PAREJA WHERE (
+			(JUGADOR_1='$this->login') OR (JUGADOR_2='$this->login')
+		)";
+
+		$result_par = $this->mysqli->query($sql_par);
+		$num_rows_par = mysqli_num_rows($result_par);
+
+		while($num_rows_par and $row_par = mysqli_fetch_array($resultado)){
+			$id_par = $row['ID'];
+
+			//Buscamos las inscripciones 
+			$sql_camp = "SELECT * FROM INSCRIPCION WHERE (PAREJA_ID = '$this->id_par')";
+			$result_camp = $this->mysqli->query($sql_camp);
+		}
+
 		//Buscamos campeonatos de las parejas
 	}
 
